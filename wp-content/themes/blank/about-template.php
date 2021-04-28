9d808b86b7836977d541595d789c92d8d22cda77
genesis679 <neagear@gmail.com>
2021-04-28 14:48:20 +0800

    First backup

---
+++ wp-content/themes/blank/about-template.php
@@ -0,0 +1,27 @@
+<?php
+/*template name: about */
+
+get_header();
+?>
+<body style="background:#fff">
+<div style="width:100%;space-between;position:relative;">
+<h2>About Us</h2> <img style="float:right;height:350px"src="http://localhost/wordpress1/wp-content/uploads/2021/04/1.jpg">  
+<p>In all chaotic beauty lies a wounded work of art.<br>
+Beautiful but torn, wreaking havoc on my heart.<br>
+Camouflaged by insecurities, blinded by it all.<br>
+I love the way you sit there and barely notice me at all.<br>
+Because I am Awesome.<br><br>
+In all chaotic beauty lies a wounded work of art.<br>
+Beautiful but torn, wreaking havoc on my heart.<br>
+Camouflaged by insecurities, blinded by it all.<br>
+I love the way you sit there and barely notice me at all.</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</div>
+</body>
+
+<?php get_footer();?>
---
+++ wp-content/themes/blank/home-template.php
@@ -0,0 +1,19 @@
+<?php
+/*template name: home */
+
+get_header();
+?>
+<body>
+<div style="background:url(http://localhost/wordpress1/wp-content/uploads/2021/04/7.jpg);background-size:cover;position:relative;width:100%;
+	min-height:100vh;justify-content:center";>
+<h2 style="color:#fff;text-align:center;font-size:5em;">Always Choose Good <br>Clothes</h2>
+<br>
+<br>
+<p style="color:#fff;text-align:center;font-size:1em;">‘Clothes maketh the man. Naked people have little or no influence on society.’</p>
+<br><br>
+<center><a style="font-size:1em;color:#fff;background:#ff0157;letter-spacing:2px;" href="Home.html">BROWSE</a></center>
+
+
+</div>
+</body>
+<?php get_footer();?>
